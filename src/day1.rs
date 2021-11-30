419d44d074af9ba967f19c68df64ea574eb80620
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2021-11-30 11:48:48 -0500

    Initial commit

---
+++ src/day1.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn parse_input_day1(input: &str) -> i32 {
+    42
+}
+
+#[aoc(day1, part1)]
+pub fn d1p1_solve(input: &i32) -> i32 {
+    12
+}
