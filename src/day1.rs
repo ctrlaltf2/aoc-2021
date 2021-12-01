8b7d75c552a82cab8bade5329e3ee04a5c4d61b7
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2021-12-01 11:51:59 -0500

    Day 01

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,36 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
-pub fn parse_input_day1(input: &str) -> i32 {
-    42
+pub fn parse_input_day1(input: &str) -> Vec<u32> {
+    input
+        .split_whitespace()
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect()
 }
 
 #[aoc(day1, part1)]
-pub fn d1p1_solve(input: &i32) -> i32 {
-    12
+pub fn d1p1_solve(input: &Vec<u32>) -> u32 {
+    let mut sum = 0;
+    for n in input.as_slice().windows(2) {
+        if n[0] < n[1] {
+            sum += 1;
+        }
+    }
+
+    sum
+}
+
+#[aoc(day1, part2)]
+pub fn d1p2_solve(input: &Vec<u32>) -> u32 {
+    let mut sum = 0;
+    for n in input.as_slice().windows(4) {
+        let a = n[0] + n[1] + n[2];
+        let b = n[1] + n[2] + n[3];
+
+        if a < b {
+            sum += 1;
+        }
+    }
+
+    sum
 }
